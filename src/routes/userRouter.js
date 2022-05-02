756836ee93fe09db514bfe7984a14cf445b0d667
nghiado.dev <nghiadv1006@gmail.com>
2022-05-02 08:07:50 +0700

    update code

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -13,7 +13,7 @@ router.patch('/user/:id/follow', auth, userCtrl.follow)
 router.patch('/user/:id/unfollow', auth, userCtrl.unfollow)
 
 router.get('/suggestionsUser', auth, userCtrl.suggestionsUser)
-
-
+router.get('/followingsUser', auth, userCtrl.followingsUser)
+router.get('/users', auth, userCtrl.getAllUsers)
 
 module.exports = router
